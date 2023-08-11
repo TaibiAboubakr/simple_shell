665bc4a2e74ab682c9e729bc6935757ead53a16b
imane <imanezenasnii@gmail.com>
2023-08-11 15:52:07 -0700

    add function that frees the occupied memory by the char pointers

--- free.c
+++ free.c
@@ -1,17 +1,35 @@
 #include "main.h"
 /**
- * _free - Frees the occupied memory by the char pointers and sets them to NULL
+ * _free - Frees the occupied memory by the char pointers
  * @a: pointer to a character array
  * @b: another character array
  */
 void _free(char *a, char *b)
 {
-	if (b)
-		free(b);
-	b = NULL;
-
 	if (a)
 		free(a);
-	a = NULL;
+
+	if (b)
+		free(b);
+}
+
+/**
+ * _free_with_null - Frees the occupied memory by the char pointers
+ * and sets them to NULL
+ * @a: pointer to a character array
+ * @b: another character array
+ */
+void _free_with_null(char **a, char **b)
+{
+	if (a && *a)
+	{
+		free(*a);
+		*a = NULL;
+	}
+	if (b && *b)
+	{
+		free(*b);
+		*b = NULL;
+	}
 }
 

2d76257519adde6aed808b58fc83f32b185207a0
imane <imanezenasnii@gmail.com>
2023-08-13 15:05:37 -0700

    Implement function to retrieve the value of an environment variable

--- env.c
+++ env.c
@@ -1,6 +1,6 @@
-#include "main.h"
+include "main.h"
 /**
- * _env - function that prints all environment variables.
+ * _env - function that prints all environment variables
  */
 void _env(void)
 {
@@ -10,5 +10,27 @@ void _env(void)
 	{	_puts(environ[i]);
 		__putchar('\n');
 	}
+}
+/**
+ * _getenv - Retrieve the value of an environment variable
+ * @var: The name of the environment variable
+ *
+ * Return: If the variable is found, a pointer to its value is returned.
+ *         Otherwise a NULL is returned
+ */
+char *_getenv(char *var)
+{
+	int i;
+	int len = _slen(var);
+
+	for (i = 0; environ[i]; i++)
+	{
+
+		if (_strncmp(environ[i], var, len) == 0 && environ[i][len] == '=')
+		{
+			return (&environ[i][len + 1]);
+		}
+	}
+	return (NULL);
 }
 

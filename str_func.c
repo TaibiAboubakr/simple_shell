45e8f97a160f8e46e5fc88e71010033f7b11f40e
aboubakr <c.aboubakr@gmail.com>
2023-08-13 23:01:59 +0100

    update function '_strncmp' to fix output errors by editing condition and return value

--- str_func.c
+++ str_func.c
@@ -22,18 +22,18 @@ int _strcmp(char *s1, char *s2)
  * characters of the two strings
  * @s1: first string to compare
  * @s2: second string to compare
+ * @n: number of the first characters to compares
  * Return: 0 if the two string are identical or number represent the difference
  * between ASCII value of the two first difference char between to strings
  */
 int _strncmp(char *s1, char *s2, int n)
 {
-    int i;
-	
-    for (i = 0; s1[i] != '\0' && s2[i] != '\0' && n >= i; i++)
-    {
-        if (s1[i] != s2[i])
-            return (s1[i] - s2[i]);
-    }
+int i;
 
-    return (s1[i] - s2[i]);
+for (i = 0; s1[i] && s2[i] && n > i; i++)
+{
+if (s1[i] != s2[i])
+return (s1[i] - s2[i]);
+}
+return (0);
 }

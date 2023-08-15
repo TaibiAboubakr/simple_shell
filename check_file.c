fe7451effb278761dce7ecadf82653a01005ad59
imane <imanezenasnii@gmail.com>
2023-08-15 06:32:48 -0700

    Update for Betty style conformity

--- check_file.c
+++ check_file.c
@@ -12,23 +12,23 @@
  */
 int check_file_exist(char *shellname, char *filename, char *err_msg, int count)
 {
-	int len;
-	struct stat f_stat;
+int len;
+struct stat f_stat;
 
-	if ((stat(filename, &f_stat) == 0) && (filename[0] == '.' || filename[0] == '/'))
-		return (1);
+if ((stat(filename, &f_stat) == 0) && (filename[0] == '.' || filename[0] == '/'))
+return (1);
 
-	len = _slen(shellname);
+len = _slen(shellname);
 
-	_puts_len(2, shellname, len);
-	write(2, ": ", 2);
-	print_number(2, count);
-	write(2, ": ", 2);
-	_puts_len(2, filename, _slen(filename));
-	write(2, ": ", 2);
-	_puts_len(2, err_msg, _slen(err_msg));
-	write(2, "\n", 1);
-	return (0);
+_puts_len(2, shellname, len);
+write(2, ": ", 2);
+print_number(2, count);
+write(2, ": ", 2);
+_puts_len(2, filename, _slen(filename));
+write(2, ": ", 2);
+_puts_len(2, err_msg, _slen(err_msg));
+write(2, "\n", 1);
+return (0);
 }
 
 /**
@@ -37,31 +37,31 @@ int check_file_exist(char *shellname, char *filename, char *err_msg, int count)
  *
  * @shellname: The name of the shell invoking the function
  * @filename: a pointer to string that contains filename
- * @err_msg: Error message to be displayed 
+ * @err_msg: Error message to be displayed
  * @count: a count associated with the current operation
  *
  * Return: 1 if file executable or 0 if not executable
  */
 int check_file_perm(char *shellname, char *filename, char *err_msg, int count)
 {
-	int len;
-	struct stat f_stat;
+int len;
+struct stat f_stat;
 
-	if ((stat(filename, &f_stat) == 0))
-		if (f_stat.st_mode & S_IXUSR)
-			return (1);
+if ((stat(filename, &f_stat) == 0))
+if (f_stat.st_mode & S_IXUSR)
+return (1);
 
-	len = _slen(shellname) - 2;
-	shellname = shellname + 2;
-	_puts_len(2, shellname, len);
-	write(2, ": ", 2);
-	print_number(2, count);
-	write(2, ": ", 2);
-	_puts_len(2, filename, _slen(filename));
-	write(2, ": ", 2);
-	_puts_len(2, err_msg, _slen(err_msg));
-	write(2, "\n", 1);
-	return (0);
+len = _slen(shellname) - 2;
+shellname = shellname + 2;
+_puts_len(2, shellname, len);
+write(2, ": ", 2);
+print_number(2, count);
+write(2, ": ", 2);
+_puts_len(2, filename, _slen(filename));
+write(2, ": ", 2);
+_puts_len(2, err_msg, _slen(err_msg));
+write(2, "\n", 1);
+return (0);
 }
 /**
  * check_file - Check the existence and permissions of a file
@@ -75,22 +75,22 @@ int check_file_perm(char *shellname, char *filename, char *err_msg, int count)
  */
 int check_file(char *shellname, char *filename, char **args, int count)
 {
-	struct stat f_stat;
+struct stat f_stat;
 
-	if (!((stat(filename, &f_stat) == 0) && (filename[0] == '.' || filename[0] == '/')))
-	{
-		print_err_file(shellname, filename, "not found", count);
-		free(args);
-		return (127);
-	}
-	/* if ((stat(filename, &f_stat) == 0)) */
-	if (!(f_stat.st_mode & S_IXUSR))
-	{
-		print_err_file(shellname, filename, "permission denied", count);
-		free(args);
-		return (126);
-	}
-	return (0);
+if (!((stat(filename, &f_stat) == 0) && (filename[0] == '.' || filename[0] == '/')))
+{
+print_err_file(shellname, filename, "not found", count);
+free(args);
+return (127);
+}
+/* if ((stat(filename, &f_stat) == 0)) */
+if (!(f_stat.st_mode & S_IXUSR))
+{
+print_err_file(shellname, filename, "permission denied", count);
+free(args);
+return (126);
+}
+return (0);
 
 }
 
@@ -99,22 +99,22 @@ int check_file(char *shellname, char *filename, char **args, int count)
  *
  * @shellname: The name of the shell invoking the function
  * @filename: the file associated with the error
- * @err_msg: Error message to be displayed 
+ * @err_msg: Error message to be displayed
  * @count: a count associated with the current operation
  */
 void print_err_file(char *shellname, char *filename, char *err_msg, int count)
 {
-	int len;
-	len = _slen(shellname);
+int len;
+len = _slen(shellname);
 
-	_puts_len(2, shellname, len);
-	write(2, ": ", 2);
-	print_number(2, count);
-	write(2, ": ", 2);
-	_puts_len(2, filename, _slen(filename));
-	write(2, ": ", 2);
-	_puts_len(2, err_msg, _slen(err_msg));
-	write(2, "\n", 1);
+_puts_len(2, shellname, len);
+write(2, ": ", 2);
+print_number(2, count);
+write(2, ": ", 2);
+_puts_len(2, filename, _slen(filename));
+write(2, ": ", 2);
+_puts_len(2, err_msg, _slen(err_msg));
+write(2, "\n", 1);
 }
 
 /**
@@ -129,6 +129,6 @@ void print_err_file(char *shellname, char *filename, char *err_msg, int count)
 
 int _putchar_err(int std, char c)
 {
-	return (write(std, &c, 1));
+return (write(std, &c, 1));
 }
 
